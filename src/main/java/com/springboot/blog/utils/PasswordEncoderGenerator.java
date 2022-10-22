4aa5103f67f8a233fc9c9b899a5a89cf40299025
gaurav <singhgaurav0410@gmail.com>
2022-10-22 21:06:07 +0530

    adding signup and login

--- src/main/java/com/springboot/blog/utils/PasswordEncoderGenerator.java
+++ src/main/java/com/springboot/blog/utils/PasswordEncoderGenerator.java
@@ -5,8 +5,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PasswordEncoderGenerator {
 
-    public static void main(){
+    public static void main(String[] args){
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode("password"));
+        System.out.println(passwordEncoder.encode("admin"));
     }
 }
